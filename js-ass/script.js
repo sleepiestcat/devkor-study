da635583a5f02bce9428e2344035b23f275dbd06
sleepiestcat <karen_hong@naver.com>
2023-04-02 08:09:23 +0900

    JS..!

--- js-ass/script.js
+++ js-ass/script.js
@@ -65,9 +65,6 @@ const checkGuess = () => {
 
 
   guesses.textContent += userGuess + ' ';
-  // TODO 
-  // 입력값에 따라 , isLow, isHigh, Collect 등의 함수를 호출 
-  // guess Count가 10이 될 경우 game over
 
   if (userGuess === randomNumber) {
     collect();
